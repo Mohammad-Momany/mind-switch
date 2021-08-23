5c8467f2db0540c9232c770b8650a1ca2c51d235
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2021-08-23 18:10:20 +0300

    display data

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import './Book.css';
 
-const Books = () => {
+const Books = ({data}) => {
+    const {title, previewLink, imageLinks, description, categories, authors, pageCount , publishedDate, publisher} = data;
     return (
     <div className="book">
-        books
+        <div className="image-contant">
+            <a target="_blank" href={previewLink} >
+                <img src={imageLinks?.thumbnail} alt={title} className="book-img" />
+            </a>
+        </div>
+    
+    <div className="book-info">
+            <h3>{title}</h3>
+    <div className="book-info-header">
+        
+    <address className="author">{authors? authors[0]: null}</address>
+    <span className="pageCount">{pageCount}</span>
+    </div>
+    <div className="publisher">{publisher}</div>
+    {/* <span>{categories}</span> */}
+    {/* <span>{publishedDate}</span> */}
+    </div>
+    <div className="book-overview">
+        <h3>Overview</h3>
+        <p>{description}</p>
+    </div>
      </div>
 )
 }
