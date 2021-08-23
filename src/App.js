91f50b2f796943a6360b0363d917135d16fafeeb
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2021-08-23 17:46:54 +0300

    fetch book data

--- src/App.js
+++ src/App.js
@@ -10,7 +10,15 @@ const [result,setResult] = useState([]);
 const [book,setBook] = useState("Javascript");
 const URL = `https://www.googleapis.com/books/v1/volumes?q=${book}&key=${APP_KEY}&maxResults=16`
 
-
+useEffect(() => {
+  getBooks()
+}, [book])
+const getBooks = async () => {
+  
+  if (!book) return setBook("Javascript")
+  const { data } = await axios.get(URL);
+  setResult(data.items);
+};
 
 
   return (
