64f822344732dfd08e1893526a98e35023439aca
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2021-08-23 17:44:49 +0300

    connected with google books api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React  from 'react'
+import React,{useState,useEffect}  from 'react'
+import axios  from 'axios'
 import './App.css';
 
 const  App=()=> {
+
+
+const APP_KEY = "AIzaSyBswzUbSbQ-eroJXoLtzRlAG8LWIMIr0k8";
+const [result,setResult] = useState([]);
+const [book,setBook] = useState("Javascript");
+const URL = `https://www.googleapis.com/books/v1/volumes?q=${book}&key=${APP_KEY}&maxResults=16`
+
+
 
 
   return (
