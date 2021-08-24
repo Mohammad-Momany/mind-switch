914c16f96efccced0bfedbabe38d9f93566da113
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2021-08-24 17:09:37 +0300

    add animation

--- src/App.js
+++ src/App.js
@@ -2,52 +2,58 @@ import React,{useState,useEffect}  from 'react'
 
 import axios  from 'axios'
 import { Input } from '@material-ui/core';
+import Books  from './components/Books/Books'
 
-import Books  from './components/Books'
-
-import './App.css';
+import './App.scss';
 
 const App = () => {
 
-
-const APP_KEY = "AIzaSyBswzUbSbQ-eroJXoLtzRlAG8LWIMIr0k8";
-const [result,setResult] = useState([]);
-const [book,setBook] = useState("Javascript");
-const URL = `https://www.googleapis.com/books/v1/volumes?q=${book}&key=${APP_KEY}&maxResults=16`
-
-useEffect(() => {
-  getBooks()
-}, [book])
-const getBooks = async () => {
-  
-  if (!book) return setBook("Javascript")
-  const { data } = await axios.get(URL);
-  setResult(data.items);
-};
+    const APP_KEY = "AIzaSyBswzUbSbQ-eroJXoLtzRlAG8LWIMIr0k8";
+    const [result,setResult] = useState([]);
+    const [book,setBook] = useState("React");
+    const URL = `https://www.googleapis.com/books/v1/volumes?q=${book}&key=${APP_KEY}&maxResults=16`;
 
 
-return (
-  <div className="app">
+    const getBooks = async () => {
 
-    <header>
+      if (!book) return setBook("Javascript");
+    
+      const { data } = await axios.get(URL);
+    
+      setResult(data.items);
+    };
+
+
+    useEffect(() => {
+    
+      getBooks()
+    
+    }, [book])
+
+
+    return (
+    
+      <div className="app">
       
-    <h1>Read book &#8594; opinion Switch </h1>
-    <Input onChange={(e) => setBook(e.target.value)}
-        placeholder="Book, Author" 
-        className="search-box" type="text"
-        inputProps={{ 'aria-label': 'description' }} 
-        required 
-      />
+        <header>
+          <h1>Read book &#8594; opinion Switch </h1>
+          <Input onChange={(e) => setBook(e.target.value)}
+            placeholder="Book, Author" 
+            className="search-box" type="text"
+            inputProps={{ 'aria-label': 'description' }} 
+            required 
+            autoComplete="false"
+          />
+        </header>
 
-    </header>
-   <div className="book-container">
-     
-   {result.map(({volumeInfo, id}) => (
-     <Books key={id} data={volumeInfo} />
-   ))}
-   </div>
+        <main className="book-container">
 
-  </div>
+          {result.map(({volumeInfo, id}) => (
+             <Books key={id} {...volumeInfo}  />
+          ))}
+        </main>
+
+      </div>
 );
 }
 
