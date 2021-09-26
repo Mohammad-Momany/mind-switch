18ed9b243be8c88a5f8c35f6be0ed17820fe710f
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2021-09-27 02:02:23 +0300

    Handling the Loading using Suspense

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { useContext } from "react";
+import { useContext, Suspense, lazy } from "react";
 
-import BooksContainer from "./components/Books_Container/BooksContainer";
 import Navbar from "./components/Navbar/Navbar";
-
 import { StateContext } from "./components/Context/BookContext";
+import Loading from "./components/LoadingAnimations/Loading";
 
 import "./App.scss";
+
+const BooksContainer = lazy(() =>
+  import("./components/Books_Container/BooksContainer")
+);
 
 const App = () => {
   const { ErrorBoundary } = useContext(StateContext);
@@ -16,7 +19,9 @@ const App = () => {
         <Navbar />
       </ErrorBoundary>
       <main className="books__container">
+        <Suspense fallback={<Loading />}>
           <BooksContainer />
+        </Suspense>
       </main>
     </>
   );
